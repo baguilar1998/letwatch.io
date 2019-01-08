3d84f1380a5e42cdb6f285a0489b364b1cbe7b85
Brian Kenji Aguilar <anjacular@gmail.com>
2019-01-08 14:58:19 -0500

    setting up backend

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-//var logger = require('morgan');
+// Package Imports
+const createError = require('http-errors');
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
 
-var apiRouter = require('./routes/book');
 
 var app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
 
-//app.use(logger('dev'));
-//app.use(express.json());
-//app.use(express.urlencoded({ extended: false }));
-//app.use(express.static(path.join(__dirname, 'dist/mean-angular6')));
-//app.use('/', express.static(path.join(__dirname, 'dist/mean-angular6')));
-//app.use('/api', apiRouter);
+// Allows servers to communicate with each other
+app.use((req, res, next) =>{
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Headers","Origin, X-Requested, Content-Type, Accept, Authorization");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, PUT, DELETE, OPTIONS");
+  next();
+});
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
