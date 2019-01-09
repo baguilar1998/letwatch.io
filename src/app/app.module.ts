0924168cb906f1c29529a860389d06f6986900a7
Brian Kenji Aguilar <anjacular@gmail.com>
2019-01-09 10:43:05 -0500

    added a few components and logo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './home-page/home-page.component';
+import { JoinRoomComponent } from './join-room/join-room.component';
+import { CreateRoomComponent } from './create-room/create-room.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomePageComponent
+    HomePageComponent,
+    JoinRoomComponent,
+    CreateRoomComponent
   ],
   imports: [
     BrowserModule,
