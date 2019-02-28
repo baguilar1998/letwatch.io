12e2c4ce5392b420d5e773e83c4ce9992281969f
Brian Kenji Aguilar <anjacular@gmail.com>
2019-02-28 09:14:10 -0500

    finished terms and conditions

--- src/app/home-page/terms-conditions/terms-conditions.component.ts
+++ src/app/home-page/terms-conditions/terms-conditions.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '../../../../node_modules/@angular/router';
 
 @Component({
   selector: 'app-terms-conditions',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TermsConditionsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  backToHome(): void {
+    this.router.navigate(['/']);
   }
 
 }
