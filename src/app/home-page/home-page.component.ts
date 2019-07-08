1f8a615c57e07c092283cb97acb6593655492d5f
Brian Kenji Aguilar <anjacular@gmail.com>
2019-07-08 18:20:14 -0400

    working on home page design

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -9,19 +9,12 @@ import { LoadingService } from '../services/loading.service';
 })
 export class HomePageComponent implements OnInit {
 
-  nickname: string;
   componentState: string;
-  currentIcon: string;
-  icons: string [];
-  index: number;
   isValid: boolean;
 
   constructor(private userService: UserService,
   private loadingService: LoadingService) {
     this.componentState = 'createroom';
-    this.icons = ['m1', 'f1', 'm2', 'f2', 'm3', 'f3', 'm4', 'f4'];
-    this.index = 0;
-    this.currentIcon = this.icons[this.index];
     this.isValid = true;
   }
 
@@ -46,34 +39,16 @@ export class HomePageComponent implements OnInit {
 
     /**
      * Creates/Updates User
-     */
+
     if (!this.userService.isUserCreated) {
       this.userService.createUser(this.nickname, this.icons[this.index]);
       this.userService.isUserCreated = true;
     } else if (this.componentState ===  'home') {
       this.userService.updateUser(this.nickname, this.icons[this.index]);
     }
-
+*/
     // Changes the component state
     this.componentState = state;
-  }
-
-
-  previousIcon() {
-    this.index--;
-    if (this.index === -1) {
-      this.index = 7;
-    }
-    this.currentIcon = this.icons[this.index];
-  }
-
-
-  nextIcon() {
-    this.index++;
-    if (this.index === 8) {
-      this.index = 0;
-    }
-    this.currentIcon = this.icons[this.index];
   }
 
 }
